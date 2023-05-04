e8d82b0654f68ac2cd32e76d039f0bc5171c8824
Guilhermeccm <guilhermematosn1@gmail.com>
2023-05-04 17:18:36 +0100

    First code added. Nothing much.

---
+++ twitter-clone/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    // BEM
+    <div className="App">
+      <h1>Hey Clever Programmers lets build a twitter clone</h1>
+      
+
+      {/*Sidebar*/}
+    <Sidebar/>
+      {/*Feed*/}
+
+      {/*Widgets*/}
+    </div> 
+  );
+}
+
+export default App;
---
+++ twitter-clone/src/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Sidebar.css'
+import HomeIcon from '@mui/icons-material/Home';
+import TwitterIcon from '@mui/icons-material/Twitter';
+function sidebar() {
+  return (
+    <div className='sidebar'>
+        {/*Twitter icon */}
+        <TwitterIcon></TwitterIcon>
+        <HomeIcon></HomeIcon>
+
+        {/*SidebarOption */}
+        {/*SidebarOption */}
+        {/*SidebarOption */}
+        {/*SidebarOption */}
+        {/*SidebarOption */}
+    </div>
+  )
+}
+
+export default sidebar
